9d7bf6560e4076e1e17aa11bea5213a3370df072
Awet Ghebrekidan <s362104@oslomet.no>
2023-11-28 11:08:26 +0100

    small fixes in test

--- Text_Generator/text_generator.py
+++ Text_Generator/text_generator.py
@@ -17,16 +17,17 @@ def filter_text(text):
 
         # Split the text into words
         words = text.split()
-        # char if char in valid_letters else char.replace(".", ". ")for char in word
         # Filter the text to keep only valid letters and spaces
         filtered_words = []
         for word in words:
+            number = re.match(pattern, word)
+            if number:
+                print(number.string)
+                in_word = num2words(number.string)
+                word = in_word
+
             filtered_word = "".join(char for char in word if char in valid_letters)
 
-            number = re.match(pattern, filtered_word)
-            if number:
-                in_word = num2words(number)
-                filtered_word.replace(number, in_word)
             filtered_words.append(filtered_word)
         filtered_text = " ".join(filtered_words)
         return filtered_text
@@ -63,7 +64,9 @@ def generator(phrase: str, num_word):
         print(e)
 
 
-"""if __name__ == "__main__":
-    generator("Sommer day", 50)
-# print(filtered_text)
-"""
+if __name__ == "__main__":
+    """filtered_text = generator("Sommer day", 10)
+    print(filtered_text)"""
+    text = "1 and two and 3"
+    filtered_text = filter_text(text)
+    print(filtered_text)
--- test/generator_test.py
+++ test/generator_test.py
@@ -17,14 +17,28 @@ model = pipeline("text-generation", model="gpt2")
 )
 def test_generator(input_param1, input_param2):
     generated_text = gen.generator(input_param1, input_param2)
+    print(generated_text)
     assert generated_text.strip() != ""
+
+
+# Define the test function
+@pytest.mark.parametrize(
+    "input_param1, input_param2",
+    [
+        ("", ""),
+    ],
+)
+def test_generator2(input_param1, input_param2):
+    generated_text = gen.generator(input_param1, input_param2)
+    assert generated_text == None
 
 
 @pytest.mark.parametrize(
     "input_param1, expected_output",
     [
-        ("Input value 1", "Input value "),
+        ("Input value 1", "Input value one"),
         ("Input value @", "Input value "),
+        ("Input value!", "Input value!")
         # Add more test cases if needed
     ],
 )
